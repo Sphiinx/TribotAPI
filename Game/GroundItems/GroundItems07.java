608fa43e3ce176646d376fb51625614e9b08ee84
Matt <deathbyman33@yahoo.com>
2016-02-05 12:06:30 -0800

    Small changes

--- Game/GroundItems/GroundItems07.java
+++ Game/GroundItems/GroundItems07.java
@@ -7,15 +7,24 @@ import org.tribot.api2007.types.RSGroundItem;
 import org.tribot.api2007.types.RSItemDefinition;
 import scripts.API.Game.Inventory.Inventory07;
 
+
 /**
  * Created by Sphiinx on 1/10/2016.
  */
 public class GroundItems07 {
 
+    /**
+     * Pickups the ground item with the specified String.
+     * @return True if the item was picked up; false otherwise.
+     * */
     public static boolean pickUpGroundItem(String... itemName) {
         return pickUpGroundItem(itemName);
     }
 
+    /**
+     * Pickups the ground item with the specified ItemID.
+     * @return True if the item was picked up; false otherwise.
+     * */
     public static boolean pickUpGroundItem(final RSGroundItem[] itemIDs) {
         if (itemIDs.length > 0) {
             for (final RSGroundItem item : itemIDs) {
